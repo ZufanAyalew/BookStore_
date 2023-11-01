566b3f06d1d4821b36fd3005790e934ce8dc355a
ZufanAyalew <zufanllw@gmail.com>
2023-11-01 12:30:30 +0300

    Add project files.

---
+++ BookStore_/Data/BookStore_Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BookStore_.Models;
+
+namespace BookStore_.Data
+{
+    public class BookStore_Context : DbContext
+    {
+        public BookStore_Context (DbContextOptions<BookStore_Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; } = default!;
+    }
+}
